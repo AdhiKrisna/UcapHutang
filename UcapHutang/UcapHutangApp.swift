fdc505607830c839f71cd923cf5c2371d2560cdc
Made Vidyatma Adhi Krisna <krisnahmbtn@gmail.com>
2026-09-11 15:17:48 +0700

    Initial Commit

---
+++ UcapHutang/UcapHutangApp.swift
@@ -0,0 +1,17 @@
+//
+//  UcapHutangApp.swift
+//  UcapHutang
+//
+//  Created by Made Vidyatma Adhi Krisna on 11/09/26.
+//
+
+import SwiftUI
+
+@main
+struct UcapHutangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
